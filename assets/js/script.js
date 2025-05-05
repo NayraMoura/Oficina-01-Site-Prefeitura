84c6b310d94baa34764e8efb7f67c6ac3f4b57a7
nayra <nayradossantosmoura@gmail.com>
2025-05-05 03:11:05 -0300

    Alterações no css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,37 @@ document.getElementById('imcForm').addEventListener('submit', function(event) {
     
     const peso = parseFloat(document.getElementById('peso').value);  
     const altura = parseFloat(document.getElementById('altura').value);  
-    
+    const resultadoDiv = document.getElementById('resultado');  
+    const imagemPersonagem = document.getElementById('imagemPersonagem');  
+
     // Verifica se os valores de peso e altura são válidos.  
-    if (peso <= 0 || altura <= 0) {  
-        resultadoDiv.innerHTML = '<p>Favor inserir valores válidos.</p>';  
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {  
+        resultadoDiv.innerHTML = '<p style="color: red;">Favor inserir valores válidos.</p>';  
         imagemPersonagem.src = 'assets/img/dados-invalidos.png';   
         imagemPersonagem.style.display = 'block'; // Exibe a imagem  
-        return;   
+        return;    
     }  
     
     const imc = (peso / (altura * altura)).toFixed(2);  
-    const resultadoDiv = document.getElementById('resultado');  
-    const imagemPersonagem = document.getElementById('imagemPersonagem');  
     
     let descricao = '';  
+    let imagemSrc = '';  
 
     if (imc < 18.5) {  
         descricao = 'Você está abaixo do peso';  
-        imagemPersonagem.src = 'assets/img/peso_abaixo.png';   
+        imagemSrc = 'assets/img/peso_abaixo.png';   
     } else if (imc < 24.9) {  
         descricao = 'Você está com peso normal';  
-        imagemPersonagem.src = 'assets/img/peso_normal.png';   
+        imagemSrc = 'assets/img/peso_normal.png';   
     } else if (imc < 29.9) {  
         descricao = 'Você está com sobrepeso';  
-        imagemPersonagem.src = 'assets/img/peso_sobre.png';   
+        imagemSrc = 'assets/img/peso_sobre.png';   
     } else {  
         descricao = 'Você está com obesidade';  
-        imagemPersonagem.src = 'assets/img/peso_acima.png';   
+        imagemSrc = 'assets/img/peso_acima.png';   
     }  
 
-    resultadoDiv.innerHTML = `<p>Seu IMC é ${imc}. Classificação: ${descricao}</p>`;  
+    resultadoDiv.innerHTML = `<p>Seu IMC é <strong>${imc}</strong>. Classificação: ${descricao}</p>`;  
+    imagemPersonagem.src = imagemSrc;  
     imagemPersonagem.style.display = 'block';  
 });
